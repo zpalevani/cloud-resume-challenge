5b00d1c81a2bdaca796c2da5a597765c760667bf
Zara Palevani <zpalevani@gmail.com>
2025-12-31 00:25:45 +0000

    update readme

--- GCP/terraform/versions.tf
+++ GCP/terraform/versions.tf
@@ -1,10 +1,18 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  cloud {
+    organization = "zarapalevani-org"
+
+    workspaces {
+      name = "gcs-zaracloudresume-gcp"
+    }
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
